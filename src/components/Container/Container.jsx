940b8356616a89e2f7396cc553621d6e678b0817
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-08-06 21:38:19 +0200

    Container-done

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,5 +1,8 @@
-const Container = () => {
-    return(<></>)
-}
+import clsx from 'clsx';
+import s from './Container.module.css';
+
+const Container = ({ children, className }) => {
+  return <div className={clsx(s.container, className)}>{children}</div>;
+};
 
 export default Container;
