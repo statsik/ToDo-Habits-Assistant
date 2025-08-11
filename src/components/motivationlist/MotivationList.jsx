c99dca3778803381b9685f9c64e36a0bde5f43d7
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-08-11 20:17:05 +0200

    Motivation-list-update

--- src/components/motivationlist/MotivationList.jsx
+++ src/components/motivationlist/MotivationList.jsx
@@ -1,6 +1,35 @@
 import css from './MotivationList.module.css'
+import { useState, useEffect } from 'react';
 
 const MotivationList = () => {
+    
+      const [quote, setQuote] = useState(null);
+      const [error, setError] = useState(null);
+
+      useEffect(() => {
+        const fetchQuote = async () => {
+        try {
+            const response = await fetch('https://zenquotes.io/api/today');
+            if (!response.ok) {
+            throw new Error('Error');
+            }
+            const data = await response.json();
+            setQuote(data[0]);
+        } catch (err) {
+            setError(err.message);
+        }
+        };
+
+        fetchQuote();
+    }, []);
+
+    if (error) {
+        return <div>Error of loading: {error}</div>;
+    }
+
+    if (!quote) {
+        return <div>Loading...</div>;
+    }
     return (
         <div className={ css["motivation-list-container"]}>
             <p className={css["p"]}>Motivation for today</p>
